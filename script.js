ac587629181c733430c598814f4fde8667c1e987
Diego Croquer <diego.croquer@gm2dev.com>
2025-09-10 10:08:10 -0300

    fix button js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hola");
+let close_button = document.getElementById("close-button");
+close_button.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("window-notice").style.display = "none";
+});
