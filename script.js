30f19861017af77dc9fe8cd75181cf53879adb85
Diego Croquer <diego.croquer@gm2dev.com>
2025-09-10 11:31:01 -0300

    mejorar estilos y clases en el modal de creación de viajes

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+//abrir modal//
 let create = document.getElementById("create-trip");
 create.addEventListener("click", function (e) {
   e.preventDefault();
   document.getElementById("window-notice").style.display = "flex";
   document.getElementById("window-notice .content").style.display = "flex";
 });
+//abrir modal//
+
+//cerrar modal//
 
 let close_create = document.getElementById("close");
 close_create.addEventListener("click", function (e) {
@@ -11,3 +15,4 @@ close_create.addEventListener("click", function (e) {
   document.getElementById("window-notice").style.display = "none";
   document.getElementById("window-notice .content").style.display = "none";
 });
+//cerrar modal//
